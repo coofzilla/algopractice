3b995034e612e43ef2ac1d9bef673fa7de212254
Jay Hernandez <jeric614@yahoo.com>
2021-10-17 21:04:19 +0900

    found most occuring character w/for in loop w/object

--- maxchar/index.js
+++ maxchar/index.js
@@ -5,6 +5,27 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+  const charsObj = {};
+  let max = 0;
+  let maxChar = '';
+
+  for (let char of str) {
+    if (!charsObj[char]) {
+      charsObj[char] = 1;
+    }
+    charsObj[char]++;
+  }
+
+  //char is assigned as the key
+  for (let char in charsObj) {
+    if (charsObj[char] > max) {
+      max = charsObj[char];
+      maxChar = char;
+    }
+
+    return maxChar;
+  }
+}
 
 module.exports = maxChar;
