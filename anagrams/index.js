c273d436883ee2136c1dd21d0ec2cd5db5d66f1e
Jay Hernandez <jeric614@yahoo.com>
2021-11-14 10:50:36 +0900

    anagram second solution w/sort

--- anagrams/index.js
+++ anagrams/index.js
@@ -41,30 +41,38 @@
 // }
 
 //helper func to build character maps
-function buildCharMap(str) {
-  const charMap = {};
+// function buildCharMap(str) {
+//   const charMap = {};
 
-  for (let char of str.replace(/[^\w]/g, '').toLowerCase()) {
-    //preincrement operator increments and returns the value after incrementing.
-    charMap[char] = ++charMap[char] || 1;
-  }
-  return charMap;
-}
+//   for (let char of str.replace(/[^\w]/g, '').toLowerCase()) {
+//     //preincrement operator increments and returns the value after incrementing.
+//     charMap[char] = ++charMap[char] || 1;
+//   }
+//   return charMap;
+// }
+
+// function anagrams(stringA, stringB) {
+//   const charMapA = buildCharMap(stringA);
+//   const charMapB = buildCharMap(stringB);
+
+//   if (Object.keys(charMapA).length !== Object.keys(charMapB).length) {
+//     return false;
+//   }
+//   //check if value at key is same, false if not
+//   for (let char in charMapA) {
+//     if (charMapA[char] !== charMapB[char]) {
+//       return false;
+//     }
+//   }
+//   return true;
+// }
 
 function anagrams(stringA, stringB) {
-  const charMapA = buildCharMap(stringA);
-  const charMapB = buildCharMap(stringB);
-
-  if (Object.keys(charMapA).length !== Object.keys(charMapB).length) {
-    return false;
-  }
-  //check if value at key is same, false if not
-  for (let char in charMapA) {
-    if (charMapA[char] !== charMapB[char]) {
-      return false;
-    }
-  }
-  return true;
+  return cleanString(stringA) === cleanString(stringB);
+}
+//sort will sort elements of an array
+function cleanString(str) {
+  return str.replace(/[^\w]/g, '').toLowerCase().split('').sort().join('');
 }
 
 module.exports = anagrams;
