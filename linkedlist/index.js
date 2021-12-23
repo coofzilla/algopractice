404f1fc8d00a3604c17503c70f3b3eb8895a8ed6
Jay Hernandez <jeric614@yahoo.com>
2021-12-23 09:40:44 +0900

    clear and remove first f/DLL

--- linkedlist/index.js
+++ linkedlist/index.js
@@ -32,6 +32,25 @@ class LinkedList {
   getFirst() {
     return this.head;
   }
+
+  getLast() {
+    if (!this.head) return null;
+    let node = this.head;
+
+    while (node) {
+      if (!node.next) return node;
+      node = node.next;
+    }
+  }
+
+  clear() {
+    this.head = null;
+  }
+
+  removeFirst() {
+    if (!this.head) return null;
+    this.head = this.head.next;
+  }
 }
 
 module.exports = { Node, LinkedList };
